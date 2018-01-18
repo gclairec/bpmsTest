bc296f7eb5ae6ca4f62b239b8fd2794ad94ac4c8
GClaireC <itsmegillianc@gmail.com>
2018-01-18 13:43:03 +0800

    Initial Commit Working logi registration firebase auth store data to DB

---
+++ employeeMGMT/MGMT/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\claire\desktop\thesis\finalthesis\employeemgmt\employeemgmt\mgmt\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
